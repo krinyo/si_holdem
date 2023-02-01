7e2d6de77b69469238054181b144d209c4e9a607
Krinyo from the office <test@test.com>
2023-02-01 22:00:39 +0600

    a lot of changes

--- second.c
+++ second.c
@@ -28,9 +28,14 @@
 #define MAX_ATTEMPTS    5
 #define START_MONEY     5000
 #define BUFSIZE         256
-#define PLAYER_CARDS    2
+#define CLIENT_CARDS    2
+#define START_SES_MON   0
+
+#define MAX_MSG_SIZE    15
+#define MAX_TABLE_CARDS 5
 
 #include <ncurses.h>
+
 /*DEBUG*/
 #ifdef DEBUG
 #define DEBUG_PRINT     1
@@ -131,11 +136,15 @@ struct client{
     struct sockaddr_in address;
     int descriptor;
     int money;
+    int session_money;
     char nickname[NICKNAME_SIZE];
     int connected;
+    int is_folded;
     
-    struct card cards[PLAYER_CARDS];
-    
+    struct card cards[CLIENT_CARDS];
+    unsigned int turn_money;
+    int combo_num;
+
     FILE *fd;
     SCREEN *scr;
     WINDOW *win;
@@ -183,58 +192,23 @@ struct server *init_server()
 }
 void show_server_info(struct server main_server)
 {
-    int i;
     printf("SERVER INFO\n");
     printf("Descs count %i\n", main_server.desc_count);
+    printf("END SERVER INFO\n");
+}
+void show_clients_info(struct server main_server)
+{
+    int i, j;
     for(i = 0; i < main_server.desc_count; i ++){
         printf("Descriptor %i =  %i\n", i, main_server.clients[i].descriptor);
         printf("Nickname = %s \n", main_server.clients[i].nickname);
         printf("Money = %i\n", main_server.clients[i].money);
+        printf("Session money = %i \n", main_server.clients[i].session_money);
+        for(j = 0; j < CLIENT_CARDS; j ++){
+            show_card(main_server.clients[i].cards[j]);
+        }
     }
-    printf("END SERVER INFO\n");
 }
-
-/*void get_clients(struct server *main_server)
-{
-    fd_set readfds;
-    struct timeval timeout;
-    int attempts = 0;
-    int result;
-    while(attempts < MAX_ATTEMPTS || main_server->desc_count < MIN_CONNECTIONS){
-        FD_ZERO(&readfds);
-        FD_SET(main_server->server_socket, &readfds);
-
-        timeout.tv_sec = WAIT_TIME;
-        timeout.tv_usec = 0;
-
-        result = select(main_server->server_socket + 1, &readfds, NULL, NULL, &timeout);
-        if(-1 == result){
-            printf("Error: select() failed!\n");
-        }
-        else if(0 == result){
-            printf("Timeout: No new connections.\n");
-            sleep(5);
-        }
-        else{
-            if(main_server->desc_count >= MAX_CONNECTIONS){
-                printf("REACHED MAXIMUM PLAYERS");
-                break;
-            };
-            // Accept the incoming connection
-            struct sockaddr_in client_addr;
-            socklen_t client_addr_len = sizeof(client_addr);
-            int client_socket = accept(main_server->server_socket, (struct sockaddr *) &client_addr, &client_addr_len);
-
-            // Add the new client descriptor to the main_server structure and increment the descriptor count
-            main_server->descriptors[main_server->desc_count] = client_socket;
-            main_server->desc_count++;
-            printf("ACCEPTED\n");
-            printf("DESC COUNTER = %i\n", main_server->desc_count);
-            sleep(5);
-        }
-        attempts++;
-    }
-}*/
 void get_clients(struct server *main_server)
 {
     fd_set readfds;
@@ -283,6 +257,7 @@ void get_clients(struct server *main_server)
             main_server->clients[i].descriptor = client_socket;
             strcpy(main_server->clients[i].nickname, nicknames[i]);
             main_server->clients[i].money = START_MONEY;
+            main_server->clients[i].session_money = START_SES_MON;
             main_server->clients[i].connected = 1;
             main_server->desc_count++;
             printf("ACCEPTED\n");
@@ -291,6 +266,21 @@ void get_clients(struct server *main_server)
         }
         attempts++;
     }
+}
+void get_client(struct server *main_server)
+{
+    struct sockaddr_in client_addr;
+    socklen_t client_addr_len = sizeof(client_addr);
+    int client_socket = accept(main_server->server_socket, (struct sockaddr *) &client_addr, &client_addr_len);
+    
+    main_server->desc_count ++;
+    main_server->clients[main_server->desc_count - 1].address = client_addr;
+    main_server->clients[main_server->desc_count - 1].descriptor = client_socket;
+    strcpy(main_server->clients[main_server->desc_count - 1].nickname, nicknames[main_server->desc_count - 1]);
+    main_server->clients[main_server->desc_count - 1].money = START_MONEY;
+    main_server->clients[main_server->desc_count - 1].connected = 1;
+    printf("ACCEPTED CLIENT\n");
+
 }
 void check_clients(struct server *main_server) {
 
@@ -310,23 +300,6 @@ void check_clients(struct server *main_server) {
 void setup_players_screen(struct server *main_server)
 {
     int i, curr_desc;
-    for(i = 0; i < main_server->desc_count; i ++){
-        curr_desc = main_server->clients[i].descriptor;
-        setlocale(LC_ALL, "");
-	    main_server->clients[i].fd = fdopen(curr_desc, "rw");
-		main_server->clients[i].scr = newterm(getenv("TERM"), main_server->clients[i].fd, main_server->clients[i].fd);
-		set_term(main_server->clients[i].scr);
-		main_server->clients[i].win = newwin(13, 42, 1, 2);
-        mvwprintw(main_server->clients[i].win, 13/2, 42/2, suits[0]);
-		flushinp();
-		if(wgetch(main_server->clients[i].win) == 'e'){
-            sleep(2);
-		}     
-    }
-}
-void setup_playerss_screen(struct server *main_server)
-{
-    int i, curr_desc;
     
     for(i = 0; i < main_server->desc_count; i ++){
         curr_desc = main_server->clients[i].descriptor;
@@ -340,42 +313,140 @@ void setup_playerss_screen(struct server *main_server)
         wrefresh(main_server->clients[i].win);
     }
 }
-struct player{};
-struct table{};
+struct table{
+    struct card table_cards[MAX_TABLE_CARDS];
+    unsigned int table_money;
+    int table_cards_count;
+    int winner_pos;
+    int max_bet;
 
+    char table_message[MAX_MSG_SIZE];
+};
+struct table *init_table()
+{
+    struct table *main_table = malloc(sizeof(struct table));
+    main_table->table_money = 0;
+    main_table->table_cards_count = 0;
+    main_table->winner_pos = -1;
+    main_table->max_bet = 0;
+    strcpy(main_table->table_message, "TEST MESSAGE");
+    return main_table;
+};
+void show_table_info(struct table main_table)
+{
+    int i;
+    for(i = 0; i < main_table.table_cards_count; i ++){
+        show_card(main_table.table_cards[i]);
+    }
+    printf("TABLE MONEY = %i\n", main_table.table_money);
+    printf("TABLE CARDS COUNT = %i\n", main_table.table_cards_count);
+    printf("TABLE MESSAGE %s \n", main_table.table_message);
+}
+//game funcs
+void give_cards_to_client(struct client *client, struct deck *main_deck)
+{
+    int i;
+    for(i = 0; i < CLIENT_CARDS; i ++){
+        memcpy(&(client->cards[i]), main_deck->first_card, sizeof(struct card));
+        pop_deck_card(main_deck);
+    }
+}
+void give_cards_to_all_clients(struct server *main_server, struct deck *main_deck)
+{
+    int i;
+    for(i = 0; i < main_server->desc_count; i ++){
+        give_cards_to_client(&(main_server->clients[i]), main_deck);
+    }
+}
+void put_cards_to_table(struct table *main_table, struct deck *main_deck, int count)
+{
+    int i;
+    for(i = 0; i < count; i ++){
+        memcpy(&(main_table->table_cards[main_table->table_cards_count]), main_deck->first_card, sizeof(struct card));
+        pop_deck_card(main_deck);
+        main_table->table_cards_count ++;
+    }
+}
+int client_bet(struct client *curr_client, int sum)
+{
+    if(sum <= curr_client->money && sum > 0){
+        curr_client->money -= sum;
+        curr_client->session_money += sum;
+        return 0;
+    }
+    else if(sum > curr_client->money){
+        curr_client->session_money += curr_client->money;
+        curr_client->money = 0;
+        return 0;
+    }
+    else{
+        return -1;
+    }
+}
+void handle_client_command(struct client *curr_client)
+{
+    int command, bet_money;
+    char buff[7];
 
+    while(1){
+        printf("in the cycle of command handling\n");
+        set_term(curr_client->scr);
+	    wmove(curr_client->win, 10, 2);
+        flushinp();
+        command = wgetch(curr_client->win);
+        if(command == 'b'){
+            recv(curr_client->descriptor, buff, 7, 0);
+            printf("%s\n", buff);
+            if((bet_money = atoi(buff)) != 0){
+                client_bet(curr_client, bet_money);
+                printf("%i\n", bet_money);
+                break;
+            }
+        }
+        else if(command == 'c'){
+            printf("checked\n");
+            break;
+        }
+        else if(command == 'f'){
+            printf("folded\n");
+            curr_client->is_folded = 1;
+            break;
+        }
+        else{
+
+        }
+    }
+}
+void client_fold(struct client *curr_client, struct table *main_table)
+{
+    curr_client->is_folded = 1;
+}
+int client_check(struct client *curr_client)
+{
+    //turn goes to next player
+    return 0;
+}
 int main()
 {
     srand(time(NULL));
 
-    //struct deck *main_deck = init_deck();
-    //fill_deck(main_deck);
-    //show_all_deck_cards(*main_deck);
     struct server *main_server = init_server();
-
-    //initscr(); // инициализация ncurses
-    //printw("\u2603"); // вывод символа "☃"
-    //mvprintw(LINES/2, COLS/2, suits[0]); // перемещение курсора в центр экрана и вывод символа "☃"
-    //refresh(); // обновление экрана
-    //getch(); // ожидание нажатия клавиши
-    //endwin(); // завершение работы ncurses
-    //return 0;
-    printf("%s\n", suits[0]);
-    get_clients(main_server);
-    setup_playerss_screen(main_server);
-    //sleep(10);
-    //while(1){
-    //    show_server_info(*main_server);
-    //    sleep(5);
-    //}
-    //sleep(5);
-    //show_server_info(*main_server);
+    struct deck *main_deck = init_deck();
+    struct table *main_table = init_table();
+    fill_deck(main_deck);
     
-    //sleep(5);
-    //check_clients(main_server);
+    get_client(main_server);
+    //reset_deck(main_deck);
+    shuffle_deck(main_deck);
+    give_cards_to_all_clients(main_server, main_deck);
+    show_clients_info(*main_server);
+    show_table_info(*main_table);
 
-    //get_clients(main_server);
-    //show_server_info(*main_server);
+    setup_players_screen(main_server);
+    handle_client_command(&main_server->clients[0]);
+    //show_all_deck_cards(*main_deck);
+    show_clients_info(*main_server);
+    sleep(5);
     return 0;
 
 }
