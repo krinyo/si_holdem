7de83b268d2d50694ec5452a488ffeb12de34e4b
Krinyo from the office <test@test.com>
2023-02-04 22:22:40 +0600

    added abstract vision of the main

---
+++ game_cycle.c
@@ -0,0 +1,50 @@
+int main()
+{
+    int i;
+
+    struct server   *MS = init_server();
+    struct deck     *MD = init_deck();
+    struct table    *MT = init_table();
+
+    fill_deck(MD);
+
+    while(1){
+        get_client_descriptors(MS);
+        shuffle_deck(MD);
+        give_cards_to_all_clients(MS, MD);
+        get_blinds(MS, MT); //not ready
+        show_table_to_clients(MT);  //not ready
+
+        while(players_bets_equal(MS)){  //not ready
+            handle_client_command(MS->clients[i]);
+            show_table_to_clients(MT);
+            kick_folded_players(MS); //not ready
+        }
+
+        put_cards_to_table(MD, MT, 3);// not ready
+        
+        for(i = 0; i < MS->desc_count; i ++){
+            handle_client_command(MS->clients[i]);
+            show_table_to_clients(MT);
+            kick_folded_players(MS); //not ready
+        }
+        
+        put_cards_to_table(MD, MT, 1);// not ready
+
+        for(i = 0; i < MS->desc_count; i ++){
+            handle_client_command(MS->clients[i]);
+            show_table_to_clients(MT);
+            kick_folded_players(MS); //not ready
+        }
+
+        put_cards_to_table(MD, MT, 1);// not ready
+
+        for(i = 0; i < MS->desc_count; i ++){
+            handle_client_command(MS->clients[i]);
+            show_table_to_clients(MT);
+            kick_folded_players(MS); //not ready
+        }
+
+        find_winner(MS, MT); //not ready
+    }
+}
